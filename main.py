ccd7ed6c5d61bd7a48ff59bf3c0a76e586b9c8a4
Samardh-Kaulwar <59438730+Samardh-Kaulwar@users.noreply.github.com>
2020-03-04 23:09:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+import sys
+
+# Get user supplied values
+cascPath = "haarcascade_frontalface_default.xml"
+cap = cv2.VideoCapture(0)
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    print("Found {0} faces!".format(len(faces)))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
